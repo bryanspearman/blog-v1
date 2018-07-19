a6f37e9f97c0eca812a42eaff5b30525afd62cfe
Bryan Spearman <bryanspearman@yahoo.com>
2018-07-19 13:10:18 -0400

    uploading final app files

---
+++ blogRouter.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+const { BlogPosts } = require('./models');
+
+
+BlogPosts.create(
+        'A Test Blog Post #1', `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione facilis error dolores explicabo animi, porro aperiam vitae eveniet aspernatur nam sit consectetur doloremque non alias exercitationem quam est! Molestiae, ipsum!
+        Praesentium ad laudantium unde est quia saepe ea repudiandae! Dolorum pariatur temporibus nisi hic obcaecati eos! Hic ipsam, sapiente distinctio animi consequuntur autem.Consectetur architecto ea animi blanditiis, ex unde.
+        Eius, voluptatibus.Sequi tenetur aspernatur non, asperiores reprehenderit eius, quia dolore autem architecto explicabo similique doloremque ipsam ipsa aliquam distinctio dolorem error quos soluta cum quisquam sint perferendis.Laudantium, ullam?`, 'Bryan Spearman');
+Recipes.create(
+        'TEST Blog Post #2', `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione facilis error dolores explicabo animi, porro aperiam vitae eveniet aspernatur nam sit consectetur doloremque non alias exercitationem quam est! Molestiae, ipsum!
+        Praesentium ad laudantium unde est quia saepe ea repudiandae! Dolorum pariatur temporibus nisi hic obcaecati eos! Hic ipsam, sapiente distinctio animi consequuntur autem.Consectetur architecto ea animi blanditiis, ex unde.
+        Eius, voluptatibus.Sequi tenetur aspernatur non, asperiores reprehenderit eius, quia dolore autem architecto explicabo similique doloremque ipsam ipsa aliquam distinctio dolorem error quos soluta cum quisquam sint perferendis.Laudantium, ullam?`, 'Bryan Spearman');
+
+
+router.get('/blog-posts', (req, res) => {
+    res.json(BlogPosts.get());
+});
+
+
+router.post('/blog-posts', jsonParser, (req, res) => {
+     const requiredFields = ['title', 'content', 'author'];
+    for (let i = 0; i < requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+    const item = BlogPosts.create(req.body.title, req.body.content, req.body.author);
+    res.status(201).json(item);
+});
+
+
+router.delete('/blog-posts/:id', (req, res) => {
+    BlogPosts.delete(req.params.id);
+    console.log(`Deleted blog post \`${req.params.ID}\``);
+    res.status(204).end();
+});
+
+
+router.put('/blog-posts/:id', jsonParser, (req, res) => {
+    const requiredFields = ['title', 'content', 'id'];
+    for (let i = 0; i < requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+    if (req.params.id !== req.body.id) {
+        const message = (
+            `Request path id (${req.params.id}) and request body id `
+                `(${req.body.id}) must match`);
+        console.error(message);
+        return res.status(400).send(message);
+    }
+    console.log(`Updating blog post \`${req.params.id}\``);
+    const updatedItem = BlogPosts.update({
+        id: req.params.id,
+        title: req.body.title,
+        content: req.body.content
+    });
+    res.status(204).end();
+})
+
+module.exports = router;
--- models.js
+++ models.js
@@ -1,22 +1,13 @@
 const uuid = require('uuid');
 
-// This module provides volatile storage, using a `BlogPost`
-// model. We haven't learned about databases yet, so for now
-// we're using in-memory storage. This means each time the app stops, our storage
-// gets erased.
-
-// Don't worry too much about how BlogPost is implemented.
-// Our concern in this example is with how the API layer
-// is implemented, and getting it to use an existing model.
-
-
 function StorageException(message) {
   this.message = message;
   this.name = "StorageException";
 }
 
 const BlogPosts = {
-  create: function (title, content, author, publishDate) {
+  create: function (title, content, author, publishDate) { 
+  console.log('Creating new blog post');
     const post = {
       id: uuid.v4(),
       title: title,
@@ -28,18 +19,15 @@ const BlogPosts = {
     return post;
   },
   get: function (id = null) {
-    // if id passed in, retrieve single post,
-    // otherwise send all posts.
     if (id !== null) {
       return this.posts.find(post => post.id === id);
     }
-    // return posts sorted (descending) by
-    // publish date
     return this.posts.sort(function (a, b) {
       return b.publishDate - a.publishDate
     });
   },
   delete: function (id) {
+    console.log(`Deleting blog post \`${id}\``);
     const postIndex = this.posts.findIndex(
       post => post.id === id);
     if (postIndex > -1) {
@@ -47,6 +35,7 @@ const BlogPosts = {
     }
   },
   update: function (updatedPost) {
+    console.log(`Updating blog post \`${updatedPost.id}\``);
     const { id } = updatedPost;
     const postIndex = this.posts.findIndex(
       post => post.id === updatedPost.id);
