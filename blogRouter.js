4c394351e1cf261bdb93aaad244eaead51f55aff
Bryan Spearman <bryanspearman@yahoo.com>
2018-07-20 20:41:53 -0400

    fixed router url

--- blogRouter.js
+++ blogRouter.js
@@ -6,21 +6,21 @@ const { BlogPosts } = require('./models');
 
 
 BlogPosts.create(
-        'A Test Blog Post #1', `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione facilis error dolores explicabo animi, porro aperiam vitae eveniet aspernatur nam sit consectetur doloremque non alias exercitationem quam est! Molestiae, ipsum!
-        Praesentium ad laudantium unde est quia saepe ea repudiandae! Dolorum pariatur temporibus nisi hic obcaecati eos! Hic ipsam, sapiente distinctio animi consequuntur autem.Consectetur architecto ea animi blanditiis, ex unde.
-        Eius, voluptatibus.Sequi tenetur aspernatur non, asperiores reprehenderit eius, quia dolore autem architecto explicabo similique doloremque ipsam ipsa aliquam distinctio dolorem error quos soluta cum quisquam sint perferendis.Laudantium, ullam?`, 'Bryan Spearman');
-Recipes.create(
-        'TEST Blog Post #2', `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione facilis error dolores explicabo animi, porro aperiam vitae eveniet aspernatur nam sit consectetur doloremque non alias exercitationem quam est! Molestiae, ipsum!
-        Praesentium ad laudantium unde est quia saepe ea repudiandae! Dolorum pariatur temporibus nisi hic obcaecati eos! Hic ipsam, sapiente distinctio animi consequuntur autem.Consectetur architecto ea animi blanditiis, ex unde.
-        Eius, voluptatibus.Sequi tenetur aspernatur non, asperiores reprehenderit eius, quia dolore autem architecto explicabo similique doloremque ipsam ipsa aliquam distinctio dolorem error quos soluta cum quisquam sint perferendis.Laudantium, ullam?`, 'Bryan Spearman');
+        'A Test Blog Post #1', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione facilis error dolores explicabo animi, porro aperiam vitae eveniet aspernatur nam sit consectetur doloremque non alias exercitationem quam est! Molestiae, ipsum!' + 
+        'Praesentium ad laudantium unde est quia saepe ea repudiandae! Dolorum pariatur temporibus nisi hic obcaecati eos! Hic ipsam, sapiente distinctio animi consequuntur autem.Consectetur architecto ea animi blanditiis, ex unde.' +
+        'Eius, voluptatibus.Sequi tenetur aspernatur non, asperiores reprehenderit eius, quia dolore autem architecto explicabo similique doloremque ipsam ipsa aliquam distinctio dolorem error quos soluta cum quisquam sint perferendis.Laudantium, ullam?', 'Bryan Spearman', Date.now());
+BlogPosts.create(
+        'TEST Blog Post #2', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione facilis error dolores explicabo animi, porro aperiam vitae eveniet aspernatur nam sit consectetur doloremque non alias exercitationem quam est! Molestiae, ipsum!' +
+        'Praesentium ad laudantium unde est quia saepe ea repudiandae! Dolorum pariatur temporibus nisi hic obcaecati eos! Hic ipsam, sapiente distinctio animi consequuntur autem.Consectetur architecto ea animi blanditiis, ex unde.' +
+        'Eius, voluptatibus.Sequi tenetur aspernatur non, asperiores reprehenderit eius, quia dolore autem architecto explicabo similique doloremque ipsam ipsa aliquam distinctio dolorem error quos soluta cum quisquam sint perferendis.Laudantium, ullam?', 'Bryan Spearman', Date.now());
 
 
-router.get('/blog-posts', (req, res) => {
+router.get('/', (req, res) => {
     res.json(BlogPosts.get());
 });
 
 
-router.post('/blog-posts', jsonParser, (req, res) => {
+router.post('/', jsonParser, (req, res) => {
      const requiredFields = ['title', 'content', 'author'];
     for (let i = 0; i < requiredFields.length; i++) {
         const field = requiredFields[i];
@@ -35,14 +35,14 @@ router.post('/blog-posts', jsonParser, (req, res) => {
 });
 
 
-router.delete('/blog-posts/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
     BlogPosts.delete(req.params.id);
     console.log(`Deleted blog post \`${req.params.ID}\``);
     res.status(204).end();
 });
 
 
-router.put('/blog-posts/:id', jsonParser, (req, res) => {
+router.put('/:id', jsonParser, (req, res) => {
     const requiredFields = ['title', 'content', 'id'];
     for (let i = 0; i < requiredFields.length; i++) {
         const field = requiredFields[i];
